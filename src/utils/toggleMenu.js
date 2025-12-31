9520a6a4892df7097547da5a24e65e9906063380
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-31 01:12:16 -0300

    feat: improve initToggleMenu function

--- src/utils/toggleMenu.js
+++ src/utils/toggleMenu.js
@@ -6,11 +6,14 @@ export function initToggleMenu() {
 	window.addEventListener("click", (e) => {
 		const isClickBtnToggleMenu = e.target.closest(".toggle-menu");
 		const isClickInsideNav = e.target.closest(".header__nav");
+		const isCLickInsideHeaderItem = e.target.closest(".header__item");
 
 		if (isClickBtnToggleMenu) {
 			nav.classList.toggle("hidden");
 			return;
 		}
+
+		if (isCLickInsideHeaderItem) nav.classList.add("hidden");
 
 		if (!nav.classList.contains("hidden") && !isClickInsideNav) nav.classList.add("hidden");
 	});
