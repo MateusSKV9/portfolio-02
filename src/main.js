5efdf941339531f94c396f4820b3ea77b3e8816c
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-30 23:41:16 -0300

    refactor: separate toggleMenu logic and adjust styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { projects, renderProjects } from "./features/project";
 import { initLoader } from "./utils/loader";
 import { initActiveMenuObserver } from "./utils/scroll";
 import { applySavedTheme, initTheme } from "./utils/theme";
+import { initToggleMenu, initWindowResize } from "./utils/toggleMenu";
 
 applySavedTheme();
 
@@ -16,47 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
 	initActiveMenuObserver();
 	initLoader();
 
-	const btn = document.querySelector(".toggle-menu");
-	const nav = document.querySelector(".header__nav");
-	const header = document.querySelector(".header");
-	btn.addEventListener("click", () => {
-		nav.classList.toggle("hidden");
-		if (!nav.classList.contains("hidden")) {
-			header.classList.remove("active");
-		} else {
-			header.classList.add("active");
-		}
-	});
-
-	window.addEventListener("resize", () => {
-		const toggleMenu = document.querySelector(".toggle-menu");
-		if (window.innerWidth > 600) {
-			toggleMenu.classList.add("hidden");
-			nav.classList.remove("hidden");
-		} else {
-			toggleMenu.classList.remove("hidden");
-		}
-	});
-
-	const toggleMenu = document.querySelector(".toggle-menu");
-	if (window.innerWidth > 600) {
-		toggleMenu.classList.add("hidden");
-		nav.classList.remove("hidden");
-	} else {
-		toggleMenu.classList.remove("hidden");
-	}
-
-	// window.addEventListener("click", (e) => {
-	// 	// Verifica se o clique foi na nav ou em qualquer coisa DENTRO da nav
-	// 	const isClickInsideNav = e.target.closest(".header_nav");
-
-	// 	if (isClickInsideNav) {
-	// 		// Clique foi dentro da nav
-	// 		console.log("Clicou dentro!");
-	// 	} else {
-	// 		// Clique foi fora da nav (útil para fechar menus)
-	// 		nav.classList.add("hidden");
-	// 		header.classList.remove("active");
-	// 	}
-	// });
+	initToggleMenu();
+	initWindowResize();
 });
---
+++ src/utils/toggleMenu.js
@@ -0,0 +1,33 @@
+export function initToggleMenu() {
+	const nav = document.querySelector(".header__nav");
+
+	setVisibilityBtnToggleMenu();
+
+	window.addEventListener("click", (e) => {
+		const isClickBtnToggleMenu = e.target.closest(".toggle-menu");
+		const isClickInsideNav = e.target.closest(".header__nav");
+
+		if (isClickBtnToggleMenu) {
+			nav.classList.toggle("hidden");
+			return;
+		}
+
+		if (!nav.classList.contains("hidden") && !isClickInsideNav) nav.classList.add("hidden");
+	});
+}
+
+export function initWindowResize() {
+	window.addEventListener("resize", setVisibilityBtnToggleMenu);
+}
+
+function setVisibilityBtnToggleMenu() {
+	const nav = document.querySelector(".header__nav");
+
+	const toggleMenu = document.querySelector(".toggle-menu");
+	if (window.innerWidth > 600) {
+		toggleMenu.classList.add("hidden-icon");
+		nav.classList.remove("hidden");
+	} else {
+		toggleMenu.classList.remove("hidden-icon");
+	}
+}
